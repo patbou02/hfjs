a4e070e367b86de3883214650928b35bb4a2d405
Patrick Boutin <patrick.boutin@roberthalf.com>
2018-11-19 22:52:37 -0800

    add hit detection code.

--- chapter2/battleship.js
+++ chapter2/battleship.js
@@ -13,9 +13,13 @@ let isSunk = false;
 // Game logic starts
 while (isSunk === false) {
   guess = prompt("Ready, aim, fire! (enter a number 0-6):");
+
   if (guess < 0 || guess > 6) {
     alert("Please enter a valid cell number!");
   } else {
     guess++;
+    if (guess === location1 || guess === location2 || guess === location3) {
+      hits++;
+    }
   }
 }
