e1ad98df4c702883dc0d716f68ca7e15351e253b
Patrick Boutin <patrick.boutin@roberthalf.com>
2018-11-19 22:04:13 -0800

    add initial variables.

--- chapter2/battleship.js
+++ chapter2/battleship.js
@@ -1,2 +1,11 @@
 // Battleship script
 console.info('Play Battleship!');
+
+// Define initial variables
+let location1 = 3;
+let location2 = 4;
+let location3 = 5;
+let guess;
+let guesses = 0;
+let hits = 0;
+let isSunk = false;
