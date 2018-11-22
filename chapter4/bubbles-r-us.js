9680a695d1bbef494accc9e2befb1cd258c42587
patbou02 <patbou02@example.com>
2018-11-22 10:53:23 -0800

    move for loop into its own function.

--- chapter4/bubbles-r-us.js
+++ chapter4/bubbles-r-us.js
@@ -7,9 +7,11 @@ console.info('Bubbles-R-Us');
 let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 let totalTests = scores.length;
 
-for (let i = 0; i < scores.length; i++) {
-  let messageOutput = `Bubbles solutions #${i} score: ${scores[i]}.`;
-  console.log(messageOutput);
+function outputScores(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let messageOutput = `Bubbles solutions #${i} score: ${arr[i]}.`;
+    console.log(messageOutput);
+  }
 }
 
 console.log(`Bubbles test: ${totalTests}`);
