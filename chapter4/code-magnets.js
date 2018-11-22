126f4cc245c574405f104407ef2c3f80f9e8d455
patbou02 <patbou02@example.com>
2018-11-22 11:12:58 -0800

    add logic to output console.log message only if hasBubbleGum is true.

--- chapter4/code-magnets.js
+++ chapter4/code-magnets.js
@@ -3,3 +3,14 @@
 "use strict";
 
 console.info('Code Magnets');
+
+let products = ["Choo choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
+let hasBubbleGum = [false, false, false, true];
+
+let i = 0;
+while (i < hasBubbleGum.length) {
+  if (hasBubbleGum[i]) {
+    console.log(products[i] + ' contains bubble gum.');
+  }
+  i++;
+}
