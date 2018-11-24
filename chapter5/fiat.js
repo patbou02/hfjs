cd55ccb77d2b4c58f543e2921acea5beb718e9ac
patbou02 <patbou02@example.com>
2018-11-23 19:00:46 -0800

    add start, stop and drive methods.

--- chapter5/fiat.js
+++ chapter5/fiat.js
@@ -11,4 +11,18 @@ let fiat = {
   passengers: 2,
   convertible: false,
   mileage: 88000,
+  started: false,
+  start: function() {
+    started = true;
+  },
+  stop: function() {
+    started = false;
+  },
+  drive: function() {
+    if (started) {
+      console.log("Zoom zoom!");
+    } else {
+      console.log("You need to start the engine first.");
+    }
+  }
 };
