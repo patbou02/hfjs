549f86384747593c4b93c33d6646374a6dac6fa0
patbou02 <patbou02@example.com>
2018-11-23 18:58:35 -0800

    add fiat HTML and JS files.

---
+++ chapter5/fiat.js
@@ -0,0 +1,14 @@
+// Fiat Exercise Script
+"use strict";
+
+console.info('Fiat');
+
+let fiat = {
+  make: "Fiat",
+  model: "500",
+  year: 1957,
+  color: "Medium Blue",
+  passengers: 2,
+  convertible: false,
+  mileage: 88000,
+};
