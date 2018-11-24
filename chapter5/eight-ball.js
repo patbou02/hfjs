fe30d0843976c2af68263be8bf41ca8db494e17e
patbou02 <patbou02@example.com>
2018-11-23 22:25:03 -0800

    create eightBall object with shake and look mathods within it. call eightBall.shake method and log look method results.

--- chapter5/eight-ball.js
+++ chapter5/eight-ball.js
@@ -2,3 +2,20 @@
 "use strict";
 
 console.info('Eight Ball');
+
+let eightBall = {
+  index: 0,
+  advice: ["Yes", "No", "Maybe", "Not a chance"],
+  shake: function() {
+    this.index++;
+    if (this.index >= this.advice.length) {
+      this.index = 0;
+    }
+  },
+  look: function() {
+    return this.advice[this.index];
+  },
+};
+
+eightBall.shake();
+console.log(eightBall.look());
