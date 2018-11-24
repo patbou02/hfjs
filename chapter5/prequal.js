4c539cba296e61dc11ff3956713d517735a12eb7
patbou02 <patbou02@example.com>
2018-11-23 16:41:46 -0800

    create taxi object.

--- chapter5/prequal.js
+++ chapter5/prequal.js
@@ -2,3 +2,13 @@
 "use strict";
 
 console.info('Pre-Qualification');
+
+let taxi = {
+  make: "Webville Motors",
+  model: "Taxi",
+  year: 1955,
+  color: "yellow",
+  passengers: 4,
+  convertible: false,
+  mileage: 281341,
+};
