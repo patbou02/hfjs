d6d709deb9e82d5ca068720dbe73513f439ff832
patbou02 <patbou02@example.com>
2018-11-25 19:23:46 -0800

    add help comments.

--- chapter7/lie-detector.js
+++ chapter7/lie-detector.js
@@ -5,8 +5,10 @@ console.info('Lie Detector');
 
 function lieDetectorTest() {
   let lies = 0;
+
   let stolenDiamond = {};
 
+  // truthy +1
   if (stolenDiamond) {
     console.log("You stole the diamond");
     lies++;
@@ -16,28 +18,35 @@ function lieDetectorTest() {
     keysInPocket: null,
   };
 
+  // null is falsey
   if (car.keysInPocket) {
     console.log("Uh oh, guess you stole the car!");
     lies++;
   }
 
+  // undefined is falsey
   if (car.emptyGasTank) {
     console.log("You drove the car after you stole it!");
     lies++;
   }
 
   let foundYouAtTheCrimeScene = [ ];
+
+  // truthy +1
   if (foundYouAtTheCrimeScene) {
     console.log("A sure sign of guilt");
     lies++;
   }
 
+  // undefined is falsey
   if (foundYouAtTheCrimeScene[0]) {
     console.log("Caught with a stolen item!");
     lies++;
   }
 
   let yourName = " ";
+
+  // truthy +1
   if (yourName) {
     console.log("Guess you lied about your name");
     lies++;
