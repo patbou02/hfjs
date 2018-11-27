4da3e2530fbb97dd1916a5a310d465040d232258
patbou02 <patbou02@example.com>
2018-11-27 01:46:44 -0800

    implement initial VIEW within battleship JS file.

--- chapter8/battleship.js
+++ chapter8/battleship.js
@@ -2,3 +2,22 @@
 "use strict";
 
 console.info('Battleship');
+
+// VIEW
+let view = {
+  // This method takes a string message passed as an argument and displays it
+  // in the message display area.
+  displayMessage: function(msg) {
+    // to be continued
+  },
+  // This method adds a .hit class in the appropriate table TD based on a passed
+  // location argument if passed value DOES match any ship location.
+  displayHit: function(location) {
+    // to be continued
+  },
+  // This method adds a .miss class in the appropriate table TD based on a passed
+  // location argument if passed value DOESN'T match any ship location.
+  displayMiss: function(location) {
+    // to be continued
+  }
+};
