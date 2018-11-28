7f77b325fb947b77d3d61a550b8209b18af5135f
patbou02 <patbou02@example.com>
2018-11-27 23:07:12 -0800

    create model object containing multiple properties of which ships is one that contains multiple individual ship objects.

--- chapter8/battleship.js
+++ chapter8/battleship.js
@@ -26,17 +26,13 @@ let view = {
 };
 
 // MODEL
-let ships = [
-  {
-    locations: ["10", "20", "30"],
-    hits: ["", "", ""],
-  },
-  {
-    locations: ["32", "33", "34"],
-    hits: ["", "", ""],
-  },
-  {
-    locations: ["63", "64", "65"],
-    hits: ["", "", "hit"],
-  },
-];
+let model = {
+  boardSize: 7,
+  numShips: 3,
+  shipsSunk: 0,
+  shipLength: 3,
+  ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""], },
+          { locations: ["24", "34", "44"], hits: ["", "", ""], },
+          { locations: ["10", "11", "12"], hits: ["", "", ""], },],
+};
+
