2ca4b598d3b73e4b72f23f26e4d968cd4b7e8b0d
patbou02 <patbou02@example.com>
2018-12-03 00:03:48 -0800

    reorder object nesting for better readability.

--- chapter8/battleship.js
+++ chapter8/battleship.js
@@ -31,9 +31,11 @@ let model = {
   numShips: 3,
   shipsSunk: 0,
   shipLength: 3,
-  ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""], },
-          { locations: ["24", "34", "44"], hits: ["", "", ""], },
-          { locations: ["10", "11", "12"], hits: ["", "", ""], },],
+  ships: [
+    { locations: ["06", "16", "26"], hits: ["", "", ""], },
+    { locations: ["24", "34", "44"], hits: ["", "", ""], },
+    { locations: ["10", "11", "12"], hits: ["", "", ""], },
+  ],
   fire: function(guess) {
     for (let i = 0; i < this.numShips; i++) {
       let ship = this.ships[i];
