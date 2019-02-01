cc289a04224fa9fd9a5cc84314977746072c539f
patbou02 <patbou02@example.com>
2019-02-01 01:20:51 -0800

    update code within processGuess method and inserted test calls.

--- chapter8/battleship.js
+++ chapter8/battleship.js
@@ -89,7 +89,14 @@ let controller = {
   // This method takes a guess in the form of "A0", "B4" etc...
   // and converts it to an ID that is meaningful within the table TDs.
   processGuess: function(guess) {
-
+    let location = parseGuess(guess);
+    if (location) {
+      this.guesses++;
+      let hit = model.fire(location);
+      if (hit && model.shipsSunk === model.numShips) {
+        view.displayMessage(`You sank all my battleships, in ${this.guesses} guesses.`);
+      }
+    }
   }
 };
 
@@ -113,3 +120,14 @@ function parseGuess(guess) {
   }
   return null;
 }
+
+controller.processGuess("A0");
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
