ac6cefb16cff6f32c819cd4945c9b1f5be20157a
patbou02 <patbou02@example.com>
2019-02-02 16:45:16 -0800

    create collision() method.

--- chapter8/battleship.js
+++ chapter8/battleship.js
@@ -100,6 +100,17 @@ let model = {
     }
     return newShipLocations;
   },
+  collision: function(locations) {
+    for (let i = 0; i < this.numShips; i++) {
+      let ship = model.ships[i];
+      for (let j = 0; j < locations.length; j++) {
+        if (ship.locations.indexOf(locations[j]) >= 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  },
 };
 
 /*
