b77cd626234bb05336ead4f05f0afffc4296d52f
patbou02 <patbou02@example.com>
2019-02-06 19:11:03 -0800

    implement blur game.

--- chapter9/blur-game.js
+++ chapter9/blur-game.js
@@ -2,3 +2,16 @@
 "use strict";
 
 console.info('Blur Game');
+
+function init() {
+  let img = document.getElementById('zero');
+
+  img.onclick = showAnswer;
+}
+
+function showAnswer() {
+  let img = document.getElementById('zero');
+  img.src = "images/zero.jpg";
+}
+
+window.onload = init;
