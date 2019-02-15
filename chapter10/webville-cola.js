d5148490cee480eec595959fbb2cbd179e9d5e1e
patbou02 <patbou02@example.com>
2019-02-14 22:58:40 -0800

    add new functions to compare names, calories and color and then print out results in console.

--- chapter10/webville-cola.js
+++ chapter10/webville-cola.js
@@ -25,6 +25,39 @@ function compareSold(colaA, colaB) {
   }
 }
 
+// sort colas by name
+function compareName(colaA, colaB) {
+  if (colaA.name > colaB.name) {
+    return 1;
+  } else if (colaA.name === colaB.name) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+// sort colas by calories in ascending order
+function compareCalories(colaA, colaB) {
+  if (colaA.calories > colaB.calories) {
+    return 1;
+  } else if (colaA.calories === colaB.calories) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+// sort colas by color
+function compareColor(colaA, colaB) {
+  if (colaA.color > colaB.color) {
+    return 1;
+  } else if (colaA.color === colaB.color) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
 function printProducts(products) {
   for (let i = 0; i < products.length; i++) {
     console.log(
@@ -36,5 +69,14 @@ function printProducts(products) {
   }
 }
 
-products.sort(compareSold);
+products.sort(compareName);
+console.log("Products sorted by name:");
+printProducts(products);
+
+products.sort(compareCalories);
+console.log("Products sorted by calories:");
+printProducts(products);
+
+products.sort(compareColor);
+console.log("Products sorted by color:");
 printProducts(products);
