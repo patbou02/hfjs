cb590715e626467de80e605c1ac17e01e01893da
patbou02 <patbou02@example.com>
2019-02-14 22:31:50 -0800

    create webville-cola HTML and JS files with products array of Objects and compareSold function which is passed to the sort method of the products array

---
+++ chapter10/webville-cola.js
@@ -0,0 +1,30 @@
+// DEFAULT NAME Exercise Script
+"use strict";
+
+console.info('Webville Cola');
+
+let products = [
+  { name: "Grapefruit", calories: 170, color: "red", sold: 8200 },
+  { name: "Orange", calories: 160, color: "orange", sold: 12101 },
+  { name: "Cola", calories: 210, color: "caramel", sold: 25412 },
+  { name: "Diet Cola", calories: 0, color: "caramel", sold: 43922 },
+  { name: "Lemon", calories: 200, color: "clear", sold: 14983 },
+  { name: "Raspberry", calories: 180, color: "pink", sold: 9427 },
+  { name: "Root Beer", calories: 200, color: "caramel", sold: 9909 },
+  { name: "Water", calories: 0, color: "clear", sold: 62123 },
+];
+
+// sort colas by number of bottles sold in ascending order
+function compareSold(colaA, colaB) {
+  if (colaA.sold > colaB.sold) {
+    return 1;
+  } else if (colaA.sold === colaB.sold) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+products.sort(compareSold);
+
+console.log(products);
