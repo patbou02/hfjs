816276b813fe2f0c9991fb613061631fb97680d5
patbou02 <poboutin@gmail.com>
2019-03-04 12:55:08 -0800

    add HTML and JS files for the bind versus call comparison exercise.

---
+++ chapter-extras/bind-vs-call.js
@@ -0,0 +1,14 @@
+// DEFAULT NAME Exercise Script
+"use strict";
+
+console.info('Comparing CALL versus BIND');
+
+function testThis() {
+  console.log("This is: ", this);
+}
+
+let dog = {
+  name: "Fido",
+};
+
+testThis.call(dog); // <== CALLs testThis right away
