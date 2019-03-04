b6c0a082acb2c77d28d8959747597e00324fd49a
patbou02 <patbou02@test.com>
2019-03-03 21:47:41 -0800

    add DJ object with one method called playsound. create controller with start method set to call DJ.playsound every second.

--- chapter-extras/webville-lounge--this.js
+++ chapter-extras/webville-lounge--this.js
@@ -2,3 +2,16 @@
 "use strict";
 
 console.info('Keeping track of THIS');
+
+let DJ = {
+  playsound: function() {
+    console.log(`Playing ${this.sound}`);
+  },
+  sound: "bells",
+};
+
+let controller = {
+  start: function() {
+    setInterval(DJ.playsound, 1000);
+  }
+};
